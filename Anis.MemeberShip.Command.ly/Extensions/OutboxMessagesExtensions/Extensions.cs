541db8f27fe5c4b341ff2d79399616bfb0952924
Ayham <ayhamalama@gmail.com>
2024-03-08 00:27:02 +0300

    Add OutboxMessage DTO for Event

---
+++ Anis.MemeberShip.Command.ly/Extensions/OutboxMessagesExtensions/Extensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Anis.MemeberShip.Command.ly.Extensions.OutboxMessagesExtensions;
+public static class Extensions
+{
+    public static EventMessage ToEventMessage(this Event @event)
+    {
+        return new EventMessage(
+            Id: @event.Id,
+            SubscrptionId: @event.AggregateId.SubscrptionId.subscrptionId,
+            MemberId: @event.AggregateId.MemberId.memberId,
+            Sequence: @event.Sequence,
+            UserId: @event.UserId,
+            DateTime: @event.DateTime,
+            Version: @event.Version
+
+            );
+    }
+}
+public record EventMessage
+(
+    int Id,
+    string SubscrptionId,
+    string MemberId,
+    int Sequence,
+    string UserId,
+    int Version,
+    DateTime DateTime
+);
