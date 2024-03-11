f974b5e17825891c8a7f64086bff3ecc1d73f34d
Ayham <ayhamalama@gmail.com>
2024-03-11 10:38:39 +0300

    .

---
+++ Anis.MemberShip.Query.ly/Services/MemberShipService.cs
@@ -0,0 +1,14 @@
+
+namespace Anis.MemberShip.Query.ly.Services
+{
+    public class MemberShipService 
+    {
+        private readonly ILogger<MemberShipService> _logger;
+        public MemberShipService(ILogger<MemberShipService> logger)
+        {
+            _logger = logger;
+        }
+
+     
+    }
+}
