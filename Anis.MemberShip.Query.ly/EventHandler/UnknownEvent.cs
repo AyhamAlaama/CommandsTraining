c0d68ea6355d42a76db521889086ac96d8f00f38
Ayham <ayhamalama@gmail.com>
2024-03-20 13:55:00 +0300

    .

--- Anis.MemberShip.Query.ly/EventHandler/UnknownEvent.cs
+++ Anis.MemberShip.Query.ly/EventHandler/UnknownEvent.cs
@@ -4,13 +4,13 @@ public record class UnknownEvent(
     string AggregateId,
     int Sequence,
     DateTime DateTime,
-    string UserId,
+    string UserID,
     int Version
 ) : Event<object>(
     AggregateId: AggregateId,
     Sequence: Sequence,
     Data: new(),
     DateTime: DateTime,
-    UserId: UserId,
+    UserId: UserID,
     Version: Version
 );
